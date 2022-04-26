e19b9a53692b4f8382340de01806cbdbd1372ddc
Ajith-26 <104148427+Ajith-26@users.noreply.github.com>
2022-04-26 12:52:18 +0530

    Delete ThreatDetectionApplication directory

--- ThreatDetectionApplication/src/com/zoho/client/MainApp.java
+++
@@ -1,9 +0,0 @@
-package com.zoho.client;
-import com.zoho.fetchlogs.LogsAnalysis;
-
-public class MainApp {
-	public static void main(String[] args) throws Exception{
-		LogsAnalysis  service = new LogsAnalysis();
-		service.analyseLogs();
-	}
-}
--- ThreatDetectionApplication/src/com/zoho/dao/ThreatsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.zoho.dao;
-
-public interface ThreatsRepository<K,V> {
-	void save(K key, V value);
-	V find(K key);
-}
